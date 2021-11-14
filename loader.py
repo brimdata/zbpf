bc0122be246a1a3b62b99c67ef16013fc2eb2cdb
Steven McCanne <steve@mccanne.com>
2021-11-14 15:57:43 -0800

    wip

--- loader.py
+++ loader.py
@@ -1,10 +1,19 @@
-import fileinput
-import zed
+import sys, select, zed
+
+def send(lines):
+    lake.load('bpf', '\n'.join(lines))
+    print("loaded %d lines" % len(lines))
 
 lake = zed.Client()
-
-for line in fileinput.input():
-    z = line.strip()
-    if len(z) != 0:
-        print(line)
-        lake.load('bpf', line)
+lines=[]
+while True:
+    i, o, e = select.select([sys.stdin], [], [], 1)
+    if (i):
+        lines.append(sys.stdin.readline().strip())
+        if len(lines) >= 500:
+            send(lines)
+            lines = []
+    else:
+        if len(lines) > 0:
+            send(lines)
+            lines = []
